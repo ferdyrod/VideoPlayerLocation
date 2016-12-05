df0c4ae8f6209ddefd8207e200095b9ccff4ca43
Ferdy Rodriguez <ferdyrodriguez@gmail.com>
2016-12-05 18:37:56 +0100

    Initial Commit

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Ferdy Rodriguez on 12/5/16.
+//  Copyright © 2016 Ferdy Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
